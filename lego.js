212079bfde210fe830b6ee0743c9519202b67f9b
Грехов Кирилл <grehhov@mail.ru>
2016-11-02 21:13:00 +0500

    Первый вариант решения

--- lego.js
+++ lego.js
@@ -1,5 +1,15 @@
 'use strict';
 
+var PRIORITIES_FOR_FUNCS = {
+    'filterIn': 0,
+    'or': 0,
+    'and': 0,
+    'sortBy': 1,
+    'select': 1,
+    'limit': 2,
+    'format': 3
+};
+var SELECTED_FIELDS;
 /**
  * Сделано задание на звездочку
  * Реализованы методы or и and
@@ -13,7 +23,28 @@ exports.isStar = true;
  * @returns {Array}
  */
 exports.query = function (collection) {
-    return collection;
+    SELECTED_FIELDS = new Set();
+    var newCollection = collection.map(function (item) {
+       return Object.assign({}, item);
+    });
+    var funcs = [].slice.call(arguments, 1);
+    funcs = funcs.sort(function (a, b) {
+        return PRIORITIES_FOR_FUNCS[a.name] - PRIORITIES_FOR_FUNCS[b.name];
+    });
+    funcs.forEach(function (func){
+        newCollection = func(newCollection);
+    });
+    if (SELECTED_FIELDS.size) {
+        newCollection.forEach(function (people) {
+            Object.getOwnPropertyNames(people).forEach(function (name) {
+                if (!SELECTED_FIELDS.has(name)) {
+                    delete people[name];
+                }
+            })
+        });
+    }
+
+    return newCollection;
 };
 
 /**
@@ -21,7 +52,16 @@ exports.query = function (collection) {
  * @params {...String}
  */
 exports.select = function () {
-    return;
+    var fields = [].slice.call(arguments);
+
+    return function select(collection) {
+        fields.forEach(function (field) {
+            if (collection.length > 0 && collection[0].hasOwnProperty(field))
+                SELECTED_FIELDS.add(field);
+        });
+
+        return collection;
+    };
 };
 
 /**
@@ -30,9 +70,16 @@ exports.select = function () {
  * @param {Array} values – Доступные значения
  */
 exports.filterIn = function (property, values) {
-    console.info(property, values);
+    return function filterIn(collection) {
+        var newCollection = [];
+        collection.forEach(function (people) {
+            if (values.indexOf(people[property]) != -1) {
+                newCollection.push(people);
+            }
+        });
 
-    return;
+        return newCollection;
+    };
 };
 
 /**
@@ -41,9 +88,19 @@ exports.filterIn = function (property, values) {
  * @param {String} order – Порядок сортировки (asc - по возрастанию; desc – по убыванию)
  */
 exports.sortBy = function (property, order) {
-    console.info(property, order);
+    return function sortBy(collection) {
+        return collection.sort(function (a, b) {
+            var result = 0;
+            if (a[property] > b[property]) {
+                result = 1;
+            }
+            if (a[property] < b[property]) {
+                result = -1;
+            }
 
-    return;
+            return order === 'asc' ? result : -result;
+        });
+    };
 };
 
 /**
@@ -52,9 +109,14 @@ exports.sortBy = function (property, order) {
  * @param {Function} formatter – Функция для форматирования
  */
 exports.format = function (property, formatter) {
-    console.info(property, formatter);
 
-    return;
+    return function format(collection) {
+        collection.forEach(function (people) {
+            people[property] = formatter(people[property]);
+        });
+
+        return collection;
+    };
 };
 
 /**
@@ -62,9 +124,9 @@ exports.format = function (property, formatter) {
  * @param {Number} count – Максимальное количество элементов
  */
 exports.limit = function (count) {
-    console.info(count);
-
-    return;
+    return function limit(collection) {
+        return collection.slice(0, count);
+    };
 };
 
 if (exports.isStar) {
@@ -75,7 +137,20 @@ if (exports.isStar) {
      * @params {...Function} – Фильтрующие функции
      */
     exports.or = function () {
-        return;
+        var filters = [].slice.call(arguments);
+
+        return function or(collection) {
+            return filters.reduce(function (currentFilteredCollection, filter) {
+                var filteredCollection = filter(collection);
+                filteredCollection.forEach(function (item) {
+                    if (currentFilteredCollection.indexOf(item) === -1) {
+                        currentFilteredCollection.push(item);
+                    }
+                });
+
+                return currentFilteredCollection;
+            }, []);
+        };
     };
 
     /**
@@ -84,6 +159,14 @@ if (exports.isStar) {
      * @params {...Function} – Фильтрующие функции
      */
     exports.and = function () {
-        return;
+        var filters = [].slice.call(arguments);
+
+        return function and(collection) {
+            filters.forEach(function (filter) {
+                collection = filter(collection);
+            });
+
+            return collection;
+        };
     };
 }
